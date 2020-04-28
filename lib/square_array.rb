e3cbdc5a84fbb75c608a8a37260fa949f623597b
Hyojin Yoo <yoohyojin@gmail.com>
2020-04-28 14:06:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   count 0
   n_array = []
   while count<array.length
-    n_array.push(array[count]*array[count])
+    n_array.push(array[count]**2)
   end
   new_array
 end
